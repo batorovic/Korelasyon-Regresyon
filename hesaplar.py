7ad5f89cdac15118ae9bea774a808d5865f5f48c
batuhank34 <33568252+batuhank34@users.noreply.github.com>
2021-06-09 00:51:20 +0300

    korelasyon,regresyon,sse

--- hesaplar.py
+++ hesaplar.py
@@ -1,4 +1,5 @@
 import math
+from numpy import index_exp, select
 import pandas as pd
 
 
@@ -23,64 +24,26 @@ class ExcelTo:
         return self.liste
 
 
-class Formulas:
+class Formulas():
     def __init__(self, reklam, telefon, satislar):
-        # self.x1 = reklam
-        # self.x2 = telefon
-        # self.y = satislar
         self.n = len(satislar)  # hepsinin uzunlugu aynı olacagi icin 1 tanesinin lenini almak yeterli.
-
-        # self.avgX1 = self.getAvg(reklam)
-        # self.avgX2 = self.getAvg(telefon)
-        # self.avgY = self.getAvg(satislar)
-        # self.sumOfX1Y = self.sumOfXY(reklam, satislar)
-        # self.sumOfX2Y = self.sumOfXY(telefon, satislar)
-        # self.r1 = self.korelasyonCalculator(reklam)
-        # self.r2 = self.korelasyonCalculator(telefon)
         self.rData = 0
-
-        self.datas = {
-            "X1": {
-                'x1': 'Reklam',
-                'avgX': self.getAvg(reklam),
-                'sumOfXY': self.sumOfXY(reklam, satislar),
-                'sumSquareOfX': self.sumOfSquare(reklam),
-                'avgSquareOfX': self.avgSquare(reklam),
-                # 'r1': self.korelasyonCalculator('X1'),
-            },
-            'X2': {
-                'x2': 'Telefon',
-                'avgX': self.getAvg(telefon),
-                'sumOfXY': self.sumOfXY(telefon, satislar),
-                'sumSquareOfX': self.sumOfSquare(telefon),
-                'avgSquareOfX': self.avgSquare(telefon),
-                # 'r2': self.korelasyonCalculator('X2'),
-            },
-            'Y': {
-                'Y': 'Satislar',
-                'avgY': self.getAvg(satislar),
-                'sumSquareOfY': self.sumOfSquare(satislar),
-                'avgSquareOfY': self.avgSquare(satislar),
-            },
-
-        }
-
-        # self.x2 = {'x2': 'Telefon', 'avgX2': self.getAvg(telefon), 'sumofX2Y': self.sumOfXY(telefon, satislar), 'sumOfSquareX2': self.sumOfSquare(telefon), 'r2': self.korelasyonCalculator(telefon)}
-
-    def korelasyonCalculator(self, data):
-        return (self.datas[data]['sumOfXY'] - self.n * self.datas[data]['avgX']*self.datas['Y']['avgY']) / math.sqrt((self.datas[data]['sumSquareOfX'] - self.n*self.datas[data]['avgSquareOfX']) * ((self.datas['Y']['sumSquareOfY'] - self.n*self.datas['Y']['avgSquareOfY'])))
-
-    def korelasyonDecider(self):
-        if self.datas['X1']['r1'] > self.datas['X2']['r2']:
-            self.rData = self.datas['X1']['r1']
-        else:
-            self.rData = self.datas['X2']['r2']
+        self.reklam = reklam
+        self.telefon = telefon
+        self.satislar = satislar
+        self.datas = {}
 
     def getAvg(self, data):
         sum = 0
         for item in data:
             sum += item
         return sum / self.n
+
+    def sum(self, data):
+        sum = 0
+        for item in data:
+            sum += item
+        return sum
 
     def sumOfXY(self, x, y):
         sum = 0
@@ -97,6 +60,146 @@ class Formulas:
     def avgSquare(self, data):
         return self.getAvg(data) ** 2
 
+    def setDatas(self):
+        self.datas = {
+            "X1": {
+                'x1': 'Reklam',
+                'xValues': self.reklam,
+                'avgX': self.getAvg(self.reklam),
+                'sumOfX': self.sum(self.reklam),
+                'sumOfXY': self.sumOfXY(self.reklam, self.satislar),
+                'sumSquareOfX': self.sumOfSquare(self.reklam),
+                'avgSquareOfX': self.avgSquare(self.reklam),
+            },
+            'X2': {
+                'x2': 'Telefon',
+                'xValues': self.telefon,
+                'avgX': self.getAvg(self.telefon),
+                'sumOfX': self.sum(self.telefon),
+                'sumOfXY': self.sumOfXY(self.telefon, self.satislar),
+                'sumSquareOfX': self.sumOfSquare(self.telefon),
+                'avgSquareOfX': self.avgSquare(self.telefon),
+            },
+            'Y': {
+                'Y': 'Satislar',
+                'y^(Egitim Tahmini Degerler)': [],
+                'y^(Test Tahmini Degerler)': [],
+                'Y_VALUES': self.satislar,
+                'avgY': self.getAvg(self.satislar),
+                'sumOfY': self.sum(self.satislar),
+                'sumSquareOfY': self.sumOfSquare(self.satislar),
+                'avgSquareOfY': self.avgSquare(self.satislar),
+            }
+        }
+
+    def getDatas(self):
+        return self.datas
+
+
+# class Korelasyon(Formulas):
+#     def __init__(self, formulas, n, reklam, telefon, satislar):
+#         super(Korelasyon, self).__init__(reklam, telefon, satislar)
+#         self.n = n
+#         self.datas = formulas.getDatas()
+#         self.formuller = formulas
+
+#     def setKorelasyon(self):
+#         self.datas['X1']['r1'] = "%0.5f" % (self.korelasyonCalculator("X1"))
+#         self.datas['X2']['r2'] = "%0.5f" % (self.korelasyonCalculator("X2"))
+#         self.korelasyonDecider()
+
+#     def korelasyonCalculator(self, data):
+#         return (self.datas[data]['sumOfXY'] - self.n * self.datas[data]['avgX']*self.datas['Y']['avgY']) / math.sqrt((self.datas[data]['sumSquareOfX'] - self.n*self.datas[data]['avgSquareOfX']) * ((self.datas['Y']['sumSquareOfY'] - self.n*self.datas['Y']['avgSquareOfY'])))
+
+#     def korelasyonDecider(self):
+#         if self.datas['X1']['r1'] > self.datas['X2']['r2']:
+#             self.rData = self.datas['X1']['r1']
+#         else:
+#             self.rData = self.datas['X2']['r2']
+
+class Korelasyon(Formulas):
+    def __init__(self, formulas, n):
+        self.n = n
+        self.datas = formulas.getDatas()
+        self.formuller = formulas
+
+    def setKorelasyon(self):
+        self.datas['X1']['r1'] = float("%0.5f" % (self.korelasyonCalculator("X1")))
+        self.datas['X2']['r2'] = float("%0.5f" % (self.korelasyonCalculator("X2")))
+
+        self.korelasyonDecider()
+
+    def korelasyonCalculator(self, data):
+        return (self.datas[data]['sumOfXY'] - self.n * self.datas[data]['avgX']*self.datas['Y']['avgY']) / math.sqrt((self.datas[data]['sumSquareOfX'] - self.n*self.datas[data]['avgSquareOfX']) * ((self.datas['Y']['sumSquareOfY'] - self.n*self.datas['Y']['avgSquareOfY'])))
+
+    def korelasyonDecider(self):
+        if self.datas['X1']['r1'] > self.datas['X2']['r2']:
+            self.rData = self.datas['X1']['r1']
+        else:
+            self.rData = self.datas['X2']['r2']
+
+
+class Regresyon():  # Formulas#):
+    # def __init__(self, formulas, n, reklam, telefon, satislar):
+    #     super(Regresyon, self).__init__(reklam, telefon, satislar)
+    def __init__(self, formulas, n):
+
+        self.formula = formulas
+        self.n = n
+        self.selectedData = {}
+        self.datas = formulas.getDatas()
+
+        self.b = 0
+        self.a = 0
+        self.x = 1
+        self.regresyonModal = 0
+
+    def calculateRegresyon(self):
+        self.regresyonDecider()
+        self.bCalculator()
+        self.aCalculator()
+        self.calculateTahminiValue()
+
+        # print(self.b)
+        # print(self.a)
+
+    def bCalculator(self):
+        self.b = float("%.2f" % (((self.n * self.selectedData["sumOfXY"]) - (self.selectedData["sumOfX"] * self.datas['Y']["sumOfY"])) / (self.n * self.selectedData["sumSquareOfX"] - (self.selectedData["sumOfX"]) ** 2)))
+
+    def aCalculator(self):
+        self.a = float("%.2f" % (self.datas['Y']['avgY'] - (float(self.b) * float(self.selectedData['avgX']))))
+
+    def regresyonDecider(self):
+        # r'ye gore x belirleyici
+        if self.datas['X1']['r1'] > self.datas['X2']['r2']:
+            self.selectedData = self.datas['X1']
+        else:
+            self.selectedData = self.datas['X2']
+
+    def calculateTahminiValue(self):
+        for index in range(self.n):
+            self.datas['Y']['y^(Egitim Tahmini Degerler)'].append(self.a + (self.b * self.selectedData['xValues'][index]))
+            # self.tahminiValues.append(self.a + (self.b * self.selectedData['xValues'][index]))
+
+
+class SSE():
+    def __init__(self, formulas, n):
+        self.formula = formulas
+        self.n = n
+        self.datas = formulas.getDatas()
+
+    def calculateEgitimVerilerSSE(self):
+        sum = 0
+        for yi in range(self.n):
+            sum += (self.datas['Y']['Y_VALUES'][yi] - self.datas['Y']['y^(Egitim Tahmini Degerler)'][yi]) ** 2
+        print(float("%.3f" % (sum)))
+
+    def calculateTestVerilerSSE(self):
+        sum = 0
+        for yi in range(self.n):
+            sum += (self.datas['Y']['Y_VALUES'][yi] - self.datas['Y']['y^(Test Tahmini Degerler)'][yi]) ** 2
+        print(float("%.3f" % (sum)))
+
 
 def main():
 
@@ -106,17 +209,24 @@ def main():
     reklamHarcamalari = excel.convertToList(excel.dataFrame["Reklam Harcamaları(x)"])
     telefonAramaSayilari = excel.convertToList(excel.dataFrame["Telefon İle Arama Sayısı"])
     satislar = excel.convertToList(excel.dataFrame["Satışlar(USD)"])
+    # %70 egitim %30 test verisi olacak sekilde ayarlanmali yani rastgele secicez. islemler egitim verisinden olacak
+    # burda karma islemini hallederim liste uzerinden.
 
     formuller = Formulas(reklamHarcamalari, telefonAramaSayilari, satislar)
-    formuller.datas['X1']['r1'] = "%0.5f" % (formuller.korelasyonCalculator("X1"))
-    formuller.datas['X2']['r2'] = "%0.5f" % (formuller.korelasyonCalculator("X2"))
-    # formuller.datas['X1'] = {
-    #     'r1': "%0.5f" % (formuller.korelasyonCalculator("X1")),
-    #     'r2': "%0.5f" % (formuller.korelasyonCalculator("X2")),
-    # }
-    formuller.korelasyonDecider()
-    print(formuller.rData)
-    print(formuller.datas['X1'], formuller.datas['X2'])
+    formuller.setDatas()
+    # korelasyon = Korelasyon(formuller, formuller.n, reklamHarcamalari, telefonAramaSayilari, satislar)
+    korelasyon = Korelasyon(formuller, formuller.n)
+    korelasyon.setKorelasyon()
+    # regrasyon = Regresyon(formuller, formuller.n, reklamHarcamalari, telefonAramaSayilari, satislar)
+    regresyon = Regresyon(formuller, formuller.n)
+    regresyon.calculateRegresyon()
+
+    sse = SSE(formuller, formuller.n)
+    sse.calculateEgitimVerilerSSE()
+
+    print(formuller.datas["X1"])
+    print(formuller.datas["X2"])
+    print(formuller.datas["Y"])
 
 
 if __name__ == "__main__":
